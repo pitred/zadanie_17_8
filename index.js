6c91f07688c0f43d6a3827a14a69116a7de9cfb2
Piotr Nowinski <pitred007git@gmail.com>
2019-05-07 21:59:29 +0200

    Add files to project

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    if (request.method === 'GET' && request.url === '/hello') {
+        fs.readFile('./index.html', 'utf-8', function (err, data) {
+            if (err) throw err;
+            response.write(data);
+            response.end();
+        });
+    } else {
+        response.statusCode = 404;
+        response.write('<img src="https://image.freepik.com/darmowe-wektory/szablon-sieci-web-b%C5%82%C4%85d-404-z-krowy-p%C5%82ywaj%C4%85ce-w-stylu-p%C5%82aski_23-2147777436.jpg" alt="404"/>');
+        response.end();
+    }
+});
+server.listen(8080);
