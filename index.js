b2222a855bc0bc76cb89dc0785d568167373c9e7
Piotr Nowinski <pitred007git@gmail.com>
2019-05-07 22:14:51 +0200

    Fix bud

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var server = http.createServer(function (request, response) {
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
     if (request.method === 'GET' && request.url === '/hello') {
         fs.readFile('./index.html', 'utf-8', function (err, data) {
-            // if (err) throw err;
             response.write(data);
             response.end();
         });
